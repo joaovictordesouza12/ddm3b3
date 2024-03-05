4ea0a91a3b0619c3b1a54e12274fabdb440e12aa
Renan4i20 <renan.ferreira110@etec.sp.gov.br>
2024-03-05 11:44:22 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Renan dos Santos Ferreira</Text>
+      <Text>João Victor de Souza e Renan</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
